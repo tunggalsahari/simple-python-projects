4b094ed3a990dfd4171d727ab4f4acf4e9a0421f
Tunggal Sahari <tunggalsahari1@gmail.com>
2026-02-02 10:08:46 +0700

    Create even-odd.py
    Even-odd number checker written in Python.

---
+++ even-odd.py
@@ -0,0 +1,8 @@
+number = int(input("Number: "))
+
+if number % 2 == 0:
+    print(f"{number} is an even mumber")
+elif number % 4 == 0:
+    print(f"{number} is an even mumber")
+else:
+    print(f"{number} is an odd mumber")
