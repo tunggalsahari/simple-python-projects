c8f5c188730a53e731f9a64ca24a7877d5a55246
Tunggal Sahari <tunggalsahari1@gmail.com>
2026-02-02 10:14:33 +0700

    Update even-odd.py
    Fixed typos.

--- even-odd.py
+++ even-odd.py
@@ -1,8 +1,8 @@
 number = int(input("Number: "))
 
 if number % 2 == 0:
-    print(f"{number} is an even mumber")
+    print(f"{number} is an even number")
 elif number % 4 == 0:
-    print(f"{number} is an even mumber")
+    print(f"{number} is an even number")
 else:
-    print(f"{number} is an odd mumber")
+    print(f"{number} is an odd number")
