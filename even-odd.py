5c891a7e471f27c84d18e838c55931f730ca3596
Tunggal Sahari <tunggalsahari1@gmail.com>
2026-02-02 10:39:40 +0700

    Update even-odd.py
    Added while loop and solution to prevent incorrect input.

--- even-odd.py
+++ even-odd.py
@@ -1,8 +1,17 @@
-number = int(input("Number: "))
-
-if number % 2 == 0:
-    print(f"{number} is an even number")
-elif number % 4 == 0:
-    print(f"{number} is an even number")
-else:
-    print(f"{number} is an odd number")
+while True:
+    try:
+        number = int(input("Enter a number to check if it's an Even or Odd number: "))
+        
+        if number % 2 == 0:
+            print(f"{number} is an even number")
+            break
+        elif number % 4 == 0:
+            print(f"{number} is an even number")
+            break
+        else:
+            print(f"{number} is an odd number")
+            break
+        
+    except ValueError:
+        message = ("Invalid input!")
+        print(message)
